4dff224490d6675e1850ed80d9dd9b4783efe9c9
Jannis Finn Schmidt <finn.schmidt@stablab.stat.uni-muenchen.de>
2026-04-30 18:34:12 +0200

    yunet datei erstellt und model onnx

---
+++ YuNet.py
@@ -0,0 +1,8 @@
+model_name = "face_detection_yunet_2023mar.omnx"
+image_path = "C:\\Users\\jfsch\\OneDrive\\Dokumente\\privat\\ESP_Cam_Project\\images"
+model_path = "C:\\Users\\jfsch\\OneDrive\\Dokumente\\privat\\ESP_Cam_Project\\models"
+
+import cv2
+import numpy as np
+
+#image = 
